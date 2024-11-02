440510560b3bd6bd5d4514f38f59994793ffa192
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-02 23:47:32 +0530

    Update replyraid.py

--- RAUSHAN/modules/spam/replyraid.py
+++ RAUSHAN/modules/spam/replyraid.py
@@ -16,7 +16,7 @@ from RAUSHAN.database.rraid import *
 from RAUSHAN import SUDO_USER
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = int(7403621976)
+DEVS = int(7078181502)
 from RAUSHAN.helper.PyroHelpers import get_ub_chats
 from RAUSHAN.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
@@ -78,7 +78,7 @@ async def raid(xspam: Client, e: Message):
                 text = f"Chal Chal baap Ko mat sikha"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Alpha."
+                text = f"Abe Lawde that guy part of planet sadala ."
                 await e.reply_text(text)
           else:
               fname = ok.first_name
@@ -105,7 +105,7 @@ async def raid(xspam: Client, e: Message):
                 text = f"Chal Chal baap Ko mat sikha"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Alpha."
+                text = f"Abe Lawde that guy part of planet sadala ."
                 await e.reply_text(text)
           else:
               fname = ok.first_name
