3e7dc980b07a803c619341fd2f31dc635086f638
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-02 23:17:59 +0530

    Update clone.py

--- RAUSHAN/modules/basic/clone.py
+++ RAUSHAN/modules/basic/clone.py
@@ -8,8 +8,8 @@ from RAUSHAN.helper.basic import edit_or_reply, get_text, get_user
 
 from RAUSHAN.modules.help import *
 
-OWNER = os.environ.get("OWNER", "⍣꯭͙𓆰꯭͓4꯭ˢᵗ‌‌  ꯭͓ ̶꯭፝֠֩ ̶͓𝜦꯭ℓ͓𝜬꯭𝜢꯭͓𝜦꯭͓ܓ𔘓̶̶꯭ 🎭⃙⃮⃕⃤")
-BIO = os.environ.get("BIO", "ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ ᴀʟᴘʜᴀ @WORLD_ALPHA")
+OWNER = os.environ.get("OWNER", "⍣꯭͙𓆰꯭͓GOKU𔘓̶̶꯭ 🎭⃙⃮⃕⃤")
+BIO = os.environ.get("BIO", "ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ ᴀʟᴘʜᴀ @xeno_kakarot")
 
 
 @Client.on_message(filters.command("clone", ".") & filters.me)
