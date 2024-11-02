8db022b37befcd7d5454548562e6582d162e716e
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-02 23:08:34 +0530

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -205,13 +205,13 @@ RAID = [
 ]
 
 
-GRP = "@WORLD_ALPHA"
+GRP = "@mlohvdryj"
 
-GROUP = [-1002094067251]
+GROUP = [-1002009280180]
 
-VERIFIED_USERS = [6730956183]
+VERIFIED_USERS = [7526369190]
 
-Owners = "@ll_ALPHA_BABY_lll|@II_RAJA_BABU_II"
+Owners = "@xeno_kakarot"
 
 PORN = [
         "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
