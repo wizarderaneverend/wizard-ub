2c45e181b00d0a86c6f87f5282913c9edba5d1ad
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-03 21:11:26 +0530

    Update help.py

--- RAUSHAN/modules/help/help.py
+++ RAUSHAN/modules/help/help.py
@@ -38,7 +38,7 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "😈kakarot😈"
+            ac.title = "😈GOKUUSERBOT😈"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @xeno_kakarot × @lolpagalokigc •"
+            message, f"```{str(ac)}```\n• [xeno_kakarot](lolpagalokigc) •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
