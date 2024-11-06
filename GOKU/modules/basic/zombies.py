b3263dca9becd163793268421be10f328adb3c07
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-06 13:14:45 +0530

    Update zombies.py

--- GOKU/modules/basic/zombies.py
+++ GOKU/modules/basic/zombies.py
@@ -4,8 +4,8 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
 from config import CMD_HANDLER
-from JAPANESE.nxtgenhelper.basic import edit_or_reply
-from JAPANESE.nxtgenhelper.parser import mention_html, mention_markdown
+from GOKU.helper.basic import edit_or_reply
+from GOKU.helper.parser import mention_html, mention_markdown
 from .help import *
 
 @Client.on_message(filters.command(["zombies"], cmd) & filters.me)
