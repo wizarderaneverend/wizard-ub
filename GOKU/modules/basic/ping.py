39376b93517471ea18927703196c3f5f454a0897
wizarderaneverend <pampaworkstg@gmail.com>
2025-07-14 01:33:58 +0530

    Update ping.py

--- GOKU/modules/basic/ping.py
+++ GOKU/modules/basic/ping.py
@@ -81,7 +81,7 @@ async def pingme(client: Client, message: Message):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await xx.edit(
-        f"❏ **╰☞ 😈kakarot😈**\n"
+        f"❏ **╰☞ 🗿@WIZARD_ERA_NEVER_END🗿**\n"
         f"├• **╰☞ 𝐒ᴘᴇᴇᴅ**`%sms`\n"
         f"├• **╰☞ 𝐔ᴘᴛɪᴍᴇ** `{uptime}` \n"
         f"└• **╰☞ 𝐍ᴀᴍᴇ:** {client.me.mention}" % (duration)
